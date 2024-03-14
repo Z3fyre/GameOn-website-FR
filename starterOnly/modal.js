072b7c9f1aa06c17e992f5a8b5c7c915f9b8a1a8
Amdy Helali <amdy.helali@gmail.com>
2024-03-14 16:41:53 +0100

    css and html fixes, confirmation popup and modal closed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,13 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const modalForm = document.getElementById("modal-form");
+const barIcon = document.getElementById("navbar-icon");
+const btnCloseConfirm = document.querySelector(".btn-close-confirmation");
+const confirmCloseIcon = document.getElementById("confirmation-close-btn");
+
+// edit nav form
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +17,51 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+//edit nav event
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+barIcon.addEventListener("click", editNav);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// close modal event
+modalCloseBtn.addEventListener("click", closeModal);
+
+//confirmation message form
+
+function validateModalForm(event) {
+  event.preventDefault();
+
+  const successMsg = document.getElementById("confirmation-msg");
+
+  if (modalForm.checkValidity()) {
+    modalForm.reset();
+    closeModal();
+    successMsg.style.display = "flex";
+  }
+}
+
+//confirmation message event
+
+modalForm.addEventListener("submit", validateModalForm);
+
+//close confirmation form
+function closeModalConfirmation() {
+  modalbg.style.display = "none";
+  window.location.reload();
+}
+
+//close confirmation event
+
+btnCloseConfirm.addEventListener("click", closeModalConfirmation);
+confirmCloseIcon.addEventListener("click", closeModalConfirmation);
